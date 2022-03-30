aef872b1b4b3fb7c9ba3e6ce708ff40c8e077d62
JangJiYeon12 <74690590+JangJiYeon12@users.noreply.github.com>
2022-03-30 19:25:30 +0900

    1

--- src/Part_1.py
+++ src/Part_1.py
@@ -4,7 +4,7 @@ Part 1
 Chinook 데이터베이스를 이용하여 각 질문에서 명시한 요구사항을 충족하는 SQL 문을 작성합니다.
 """
 
-QUERY_1 = "1번문제의 답이 되는 SQL 쿼리문을 작성해주세요"
+QUERY_1 = "SELECT c.CoustomerID, c.City || ' ' || c.Country FROM Customers c;"
 
 QUERY_2 = "2번문제의 답이 되는 SQL 쿼리문을 작성해주세요"
 
