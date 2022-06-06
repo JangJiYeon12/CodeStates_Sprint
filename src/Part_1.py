99584f8070b49f88a670b73175f499ff79df27b8
JangJiYeon12 <1021jja@naver.com>
2022-06-06 20:34:37 +0900

    second submit

--- src/Part_1.py
+++ src/Part_1.py
@@ -4,9 +4,6 @@ Bare Minimum Requirements
 파이썬의 기본형태를 이해하고 컴프리헨션을 활용할 줄 알아야 합니다.
 
 요구사항:
-    반복해서 수학적인 개념이 접목된 프로그래밍 문제를 해결해봅니다.
-    프로그래밍에 익숙해지는 과정에는 코드 자체에 대한 맥락을 읽어야 하는 것이 중요합니다.
-
     리스트 컴프리헨션 개념을 적용하여 '1~100까지 7과 5의 공배수' 를 구하는 코드를 작성하세요.
     아래 예시입력값과 출력값을 참조하며 문제를 해결해봅니다.
 
@@ -17,11 +14,6 @@ Bare Minimum Requirements
 """
 
 def part1():
-    list = range(1, 100)
-    answer = []
+    answer = [i for i in range(1, 100) if i%35==0]
 
-    for i in list:
-        if i%35==0:
-            answer.append(i)
-    
-    print(answer)
+    return answer
